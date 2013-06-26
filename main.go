e98a02ae3eced163201710477290dbe34f0c91ea
Santiaago <santiago.ariassar@gmail.com>
2013-06-26 22:29:00 +0200

    Hello world purple-wing app
    Fixes #2

---
+++ main.go
@@ -0,0 +1,56 @@
+package hello
+
+import (
+	"html/template"
+	"net/http"
+	"appengine"
+)
+
+func init(){
+	http.HandleFunc("/", mainHandler)
+}
+
+var mainTemplate = template.Must(template.New("Main").Parse(mainHTML))
+
+const mainHTML = `
+<html>
+<body>
+<h2>hello purple-wing in Go!</h2>
+<b>Contributors:</b>
+<ul>
+<li>Santiago (sar)</li>
+</body>
+</html>
+`
+
+
+func mainHandler(w http.ResponseWriter, r *http.Request){
+	c := appengine.NewContext(r)
+	c.Infof("pw: mainHandler")
+	c.Infof("pw: Requested URL: %v", r.URL)
+
+	if err := mainTemplate.Execute(w, nil); err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
