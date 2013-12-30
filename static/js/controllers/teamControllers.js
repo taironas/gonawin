12ba93927b72dd5bb24cf93eb32e614b56619b4a
Remy Jourde <remy.jourde@gmail.com>
2013-12-30 18:12:24 +0100

    handle redirect in team controllers
    Fixes #181

--- static/js/controllers/teamControllers.js
+++ static/js/controllers/teamControllers.js
@@ -6,26 +6,43 @@ teamControllers.controller('TeamListCtrl', ['$scope', 'Team', function($scope, T
 	$scope.teams = Team.query();
 }]);
 
-teamControllers.controller('TeamNewCtrl', ['$scope', 'Team', function($scope, Team) {
+teamControllers.controller('TeamNewCtrl', ['$scope', 'Team', '$location', function($scope, Team, $location) {
 	$scope.addTeam = function() {
-		Team.save($scope.team);
-	}
-}]);
-
-teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', function($scope, $routeParams, Team) {
-	$scope.team = Team.get({ id:$routeParams.id });
-	
-	$scope.deleteTeam = function() {
-		Team.delete({ id:$routeParams.id });
+		Team.save($scope.team,
+			function(team) {
+				$location.path('/teams/show/' + team.Id);
+			},
+			function(err) {
+				console.log('save failed: ', err.data);
+			});
 	};
 }]);
 
-teamControllers.controller('TeamEditCtrl', ['$scope', '$routeParams', 'Team', function($scope, $routeParams, Team) {
+teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$location', function($scope, $routeParams, Team, $location) {
+	$scope.team = Team.get({ id:$routeParams.id });
+	
+	$scope.deleteTeam = function() {
+		Team.delete({ id:$routeParams.id },
+			function(){
+				$location.path('/');
+			},
+			function(err) {
+				console.log('delete failed: ', err.data);
+			});
+	};
+}]);
+
+teamControllers.controller('TeamEditCtrl', ['$scope', '$routeParams', 'Team', '$location', function($scope, $routeParams, Team, $location) {
 	$scope.team = Team.get({ id:$routeParams.id });
 	
 	$scope.updateTeam = function() {
 		var team = Team.get({ id:$routeParams.id });
-		
-		Team.update({ id:$routeParams.id }, $scope.team);
+		Team.update({ id:$routeParams.id }, $scope.team,
+			function(){
+				$location.path('/teams/show/' + $routeParams.id);
+			},
+		function(err) {
+			console.log('update failed: ', err.data);
+		});
 	}
 }]);
