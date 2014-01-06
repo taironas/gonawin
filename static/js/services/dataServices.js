2ddd316f1f7162f90f8f3a3c6d3db18328ae1166
Remy Jourde <remy.jourde@gmail.com>
2014-01-06 18:27:53 +0100

    client: before calling user json handlers, user session data should be passed in the request
    Fixes #164

--- static/js/services/dataServices.js
+++ static/js/services/dataServices.js
@@ -1,14 +1,18 @@
 'use strict'
 var dataServices = angular.module('dataServices', ['ngResource']);
 
-dataServices.factory('User', function($resource) {
+dataServices.factory('User', function($http, $resource, $cookieStore) {
+	$http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
+	
 	return $resource('j/users/:id', {id:'@id'}, {
 		get: { method: 'GET', url: 'j/users/show/:id' },
 		update: { method: 'POST', url: 'j/users/update/:id' },
 	})
 });
 
-dataServices.factory('Team', function($resource) {
+dataServices.factory('Team', function($http, $resource) {
+	$http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
+	
 	return $resource('j/teams/:id', {id:'@id'}, {
 		get: { method: 'GET', url: 'j/teams/show/:id' },
 		save: { method: 'POST', url: 'j/teams/new' },
@@ -17,7 +21,9 @@ dataServices.factory('Team', function($resource) {
 	})
 });
 
-dataServices.factory('Tournament', function($resource) {
+dataServices.factory('Tournament', function($http, $resource) {
+	$http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
+	
 	return $resource('j/tournaments/:id', {id:'@id'}, {
 		get: { method: 'GET', url: 'j/tournaments/show/:id' },
 		save: { method: 'POST', url: 'j/tournaments/new' },
@@ -27,23 +33,25 @@ dataServices.factory('Tournament', function($resource) {
 });
 
 dataServices.factory('Invite', function($http, $log, $q){
-    return {
-			send: function(currentUser, emails){
-				var deferred = $q.defer();
-					$http({
-						method: 'POST',
-						url: '/j/invite',
-						contentType: 'application/json',
-						params:{ emails: emails, name: currentUser.Name } }).
-						success(function(data,status,headers,config){
-							deferred.resolve(data);
-							$log.info(data, status, headers() ,config);
-						}).
-						error(function (data, status, headers, config){
-							$log.warn(data, status, headers(), config);
-							deferred.reject(status);
-						});
-					return deferred.promise;
-			}
-    };
+	$http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
+	
+	return {
+		send: function(currentUser, emails){
+			var deferred = $q.defer();
+				$http({
+					method: 'POST',
+					url: '/j/invite',
+					contentType: 'application/json',
+					params:{ emails: emails, name: currentUser.Name } }).
+					success(function(data,status,headers,config){
+						deferred.resolve(data);
+						$log.info(data, status, headers() ,config);
+					}).
+					error(function (data, status, headers, config){
+						$log.warn(data, status, headers(), config);
+						deferred.reject(status);
+					});
+				return deferred.promise;
+		}
+	};
 });
