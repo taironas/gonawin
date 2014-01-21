92e3cc920077b1eebb8b607d64094672d97d0389
santiaago <santiago.ariassar@gmail.com>
2014-01-21 21:05:52 +0100

    remove file inviteData.js as it is not use #179

--- static/js/controllers/inviteControllers.js
+++ static/js/controllers/inviteControllers.js
@@ -3,9 +3,10 @@
 var inviteControllers = angular.module('inviteControllers', []);
 
 inviteControllers.controller('InviteCtrl', ['$scope', 'Invite', 'SessionService', function($scope, Invite, SessionService) {
-	$scope.currentUser = SessionService.getCurrentUser();
-	
-	$scope.inviteFriends = function() {
-		Invite.send($scope.currentUser, $scope.invite.emails);
-	}
+    $scope.currentUser = SessionService.getCurrentUser();
+    
+    $scope.inviteFriends = function() {
+	console.log('invite friends');
+	Invite.send($scope.currentUser, $scope.invite.emails);
+    }
 }]);
