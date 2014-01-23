39f0dc9b43dcbb1bc00fda14fc3ded7a9f6f5240
Remy Jourde <remy.jourde@gmail.com>
2014-01-23 08:58:33 +0100

    directive: join button for teams should use existing
    Fixes #219

--- static/js/directives/joinButton.js
+++ static/js/directives/joinButton.js
@@ -1,11 +1,12 @@
 'use strict'
 
 angular.module('directive.joinButton', []).directive('joinbutton', [
-  'Tournament', '$compile', '$routeParams', function(Tournament, $compile, $routeParams) {
+  'Team','Tournament', '$compile', '$routeParams', function(Team, Tournament, $compile, $routeParams) {
     return {
       restrict: 'E',
       scope: {
-        target: '='
+        target: '=',    // Bind the target to the object given
+        resource: '@'   // Store the string associated by resource
       },
       replace: true,
       link: function(scope, element, attrs) {
@@ -18,13 +19,26 @@ angular.module('directive.joinButton', []).directive('joinbutton', [
           element.append(join_btn);
           return join_btn.bind('click', function(e) {
             scope.submitting = true;
-            Tournament.join({id:$routeParams.id}, function(response) {
-              scope.submitting = false;
-              join_btn.remove();
-              return createLeaveBtn();
-            }, function(error) {
-              return scope.submitting = false;
-            });
+            if(scope.resource == 'tournament')
+            {
+              Tournament.join({id:$routeParams.id}, function(response) {
+                scope.submitting = false;
+                join_btn.remove();
+                return createLeaveBtn();
+              }, function(error) {
+                return scope.submitting = false;
+              });
+            }
+            else if(scope.resource == 'team')
+            {
+              Team.join({id:$routeParams.id}, function(response) {
+                scope.submitting = false;
+                join_btn.remove();
+                return createLeaveBtn();
+              }, function(error) {
+                return scope.submitting = false;
+              });
+            }
             return scope.$apply();
           });
         };
@@ -34,13 +48,26 @@ angular.module('directive.joinButton', []).directive('joinbutton', [
           element.append(leave_btn);
           return leave_btn.bind('click', function(e) {
             scope.submitting = true;
-            Tournament.leave({id:$routeParams.id}, function(response) {
-              scope.submitting = false;
-              leave_btn.remove();
-              return createJoinBtn();
-            }, function(error) {
-              return scope.submitting = false;
-            });
+            if(scope.resource == 'tournament')
+            {
+              Tournament.leave({id:$routeParams.id}, function(response) {
+                scope.submitting = false;
+                leave_btn.remove();
+                return createJoinBtn();
+              }, function(error) {
+                return scope.submitting = false;
+              });
+            }
+            else if(scope.resource == 'team')
+            {
+              Team.leave({id:$routeParams.id}, function(response) {
+                scope.submitting = false;
+                leave_btn.remove();
+                return createJoinBtn();
+              }, function(error) {
+                return scope.submitting = false;
+              });
+            }
             return scope.$apply();
           });
         };
