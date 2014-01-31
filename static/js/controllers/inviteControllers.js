a3fa7596d41f4c5198d98d57e5ed597038e56f11
Remy Jourde <remy.jourde@gmail.com>
2014-01-31 13:44:09 +0100

    Error in invite
    Fixes #247

--- static/js/controllers/inviteControllers.js
+++ static/js/controllers/inviteControllers.js
@@ -2,17 +2,13 @@
 
 var inviteControllers = angular.module('inviteControllers', []);
 
-inviteControllers.controller('InviteCtrl', ['$scope', 'Invite', 'SessionService', function($scope, Invite, SessionService) {
-    
-    $scope.inviteFriends = function() {
-	console.log('invite friends');
-	console.log($scope.currentUser.User);
-	console.log($scope.invite.emails);
-	Invite.send({emails: $scope.invite.emails},
-		    function(result){
-			console.log('invite successfull: ');
-		    }, function(err){
-			console.log('error: ',err);
-		    });
-    };
+inviteControllers.controller('InviteCtrl', ['$scope', 'Invite', function($scope, Invite) {
+  $scope.inviteFriends = function() {
+    console.log('invite friends');
+    Invite.send({emails: $scope.invite.emails},function(result){
+      console.log('invite successfull: ');
+    }, function(err){
+      console.log('error: ',err);
+    });
+  };
 }]);
