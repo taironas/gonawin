ab96cfc7cb5644e2cf60e857a0a2a57d02636ccc
Remy Jourde <remy.jourde@gmail.com>
2014-02-20 18:42:28 +0100

    Navbar should have its own controller
    Fixes #278

---
+++ static/js/controllers/navigationControllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var navigationControllers = angular.module('navigationControllers', []);
+
+navigationControllers.controller('NavigationCtrl', ['$scope', '$location', '$cookieStore', 'Session', 'sAuth', 
+  function ($scope, $location, $cookieStore, Session, sAuth) {
+    console.log('NavigationCtrl module');
+    
+    $scope.disconnect = function(){
+      console.log('NavigationCtrl module:: disconnect');
+
+      Session.logout({ token: $cookieStore.get('access_token') });
+      sAuth.logout();
+      
+      $cookieStore.remove('auth');
+      $cookieStore.remove('access_token');
+      $cookieStore.remove('user_id');
+      
+      $location.path('/');
+    };
+}]);
