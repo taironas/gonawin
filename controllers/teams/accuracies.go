811bf09ed0dbbd9b053bf85c85b22240a56c945c
santiaago <santiago.ariassar@gmail.com>
2014-03-13 22:40:09 +0100

    add new accuracies files involves #174

---
+++ controllers/teams/accuracies.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013 Santiago Arias | Remy Jourde | Carlos Bernal
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package teams
+
+import (
+	"errors"
+	"net/http"
+
+	"appengine"
+
+	"github.com/santiaago/purple-wing/helpers"
+	"github.com/santiaago/purple-wing/helpers/handlers"
+	"github.com/santiaago/purple-wing/helpers/log"
+	templateshlp "github.com/santiaago/purple-wing/helpers/templates"
+
+	mdl "github.com/santiaago/purple-wing/models"
+)
+
+// Json team accuracies handler:
+// Use this handler to get the accuracies of a team.
+// The accuracies data response is an array of accurracies of the team group by tournament with the last 5 progressions.
+func AccuraciesJson(w http.ResponseWriter, r *http.Request, u *mdl.User) error {
+	c := appengine.NewContext(r)
+	desc := "Team Accuracies Handler:"
+
+	if r.Method == "GET" {
+		teamId, err := handlers.PermalinkID(r, c, 3)
+		if err != nil {
+			log.Errorf(c, "%s error extracting permalink err:%v", desc, err)
+			return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTeamNotFound)}
+		}
+
+		var t *mdl.Team
+		t, err = mdl.TeamById(c, teamId)
+		if err != nil {
+			log.Errorf(c, "%s team with id:%v was not found %v", desc, teamId, err)
+			return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeTeamNotFound)}
+		}
+
+		log.Infof(c, "%s ready to build a acc array", desc)
+		accs := t.AccuraciesByTournament(c)
+
+		// fieldsToKeep := []string{"Id", "Name", "Score"}
+		// usersJson := make([]mdl.UserJson, len(users))
+		// helpers.TransformFromArrayOfPointers(&users, &usersJson, fieldsToKeep)
+
+		data := struct {
+			Accuracies *[]mdl.AccuracyOverall
+		}{
+			accs,
+		}
+
+		return templateshlp.RenderJson(w, c, data)
+	}
+	return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeNotSupported)}
+}
