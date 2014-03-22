a7053cf486dfd1bca91cef89554b921f7ff14736
santiaago <santiago.ariassar@gmail.com>
2014-03-23 00:41:00 +0100

    add ranking of participants and teams to tournament dashboard #272

--- static/js/controllers/dashboardControllers.js
+++ static/js/controllers/dashboardControllers.js
@@ -3,7 +3,7 @@
 // Dashboard controllers update the dashboard information depending on the user location.
 var dashboardControllers = angular.module('dashboardControllers', []);
 
-dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', 'Session', 'sAuth', 'User', 'Team', '$route', function ($scope, $rootScope, $routeParams, $location, $cookieStore, Session, sAuth, User, Team, $route) {
+dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', 'Session', 'sAuth', 'User', 'Team', 'Tournament', '$route', function ($scope, $rootScope, $routeParams, $location, $cookieStore, Session, sAuth, User, Team, Tournament, $route) {
   console.log('Dashboard module');
   console.log('DashboardCtrl, current user = ', $rootScope.currentUser);
   console.log('DashboardCtrl, isLoggedIn = ', $rootScope.isLoggedIn);
@@ -18,6 +18,16 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
   //
   // the context variable has the current state of the dashboard.
   // context can take values 'user', 'team', 'tournament', 'default'.
+  //
+  // On RouteParams:
+  //
+  //   We are not able to get the current team id with $routeParams, so we use $route instead.
+  //   from angular documentation: 
+  //   http://docs.angularjs.org/api/ngRoute/service/$routeParams
+  //     Note that the $routeParams are only updated after a route change completes successfully. 
+  //     This means that you cannot rely on $routeParams being correct in route resolve functions. 
+  //     Instead you can use $route.current.params to access the new route's parameters.
+  //
   $scope.setDashboard = function(){
     var ctx = 'set dashboard: ';
     console.log(ctx, 'location - abs url: ', $location.absUrl());
@@ -32,6 +42,7 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
     // reset dashboard before any set.
     $scope.dashboard = {};
     if(url.match('^/$') != null){
+
       $scope.dashboard.location = 'root';
       $scope.dashboard.context = 'user';
       $rootScope.currentUser.$promise.then(function(currentUser){
@@ -43,10 +54,36 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
 	$scope.dashboard.score = currentUser.User.Score;
       });
     } else if(url.match('^/tournaments/[0-9]+.*') != null){
+
       $scope.dashboard.location = 'tournament';
       $scope.dashboard.context = 'tournament';
 
+      console.log(ctx, 'route', $route);
+      Tournament.get({ id:$route.current.params.id }).$promise.then(function(tournamentResult){
+      	console.log(ctx, 'get tournament ', tournamentResult);
+	$scope.dashboard.tournament = tournamentResult.Tournament.Name;
+	$scope.dashboard.nparticipants = tournamentResult.Participants.length;
+	$scope.dashboard.nteams = tournamentResult.Teams.length;
+      });
+      $scope.dashboard.rank = {};
+
+      Tournament.ranking({id:$route.current.params.id, rankby:'users'}).$promise.then(function(rankResult){
+	console.log(ctx, 'get users ranking ', rankResult);
+	$scope.dashboard.rank.users = rankResult.Users;
+      });
+
+      Tournament.ranking({id:$route.current.params.id, rankby:'teams'}).$promise.then(function(rankResult){
+	console.log(ctx, 'get teams ranking', rankResult);
+	$scope.dashboard.rank.teams = rankResult.Teams;
+      });
+
+      $rootScope.currentUser.$promise.then(function(currentUser){
+	$scope.dashboard.user = currentUser.User.Name;
+	$scope.dashboard.id = currentUser.User.Id;
+      });
+
     } else if(url.match('^/teams/[0-9]+.*') != null){
+
       $scope.dashboard.location = 'team';
       $scope.dashboard.context = 'team';
 
@@ -55,15 +92,10 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
 	$scope.dashboard.id = currentUser.User.Id;
       });
 
-      // We are not able to get the current team id with $routeParams, so we use $route instead.
-      // from angular documentation: 
-      // http://docs.angularjs.org/api/ngRoute/service/$routeParams
-      //   Note that the $routeParams are only updated after a route change completes successfully. 
-      //   This means that you cannot rely on $routeParams being correct in route resolve functions. 
-      //   Instead you can use $route.current.params to access the new route's parameters.
       console.log(ctx, 'route', $route);
       Team.get({ id:$route.current.params.id }).$promise.then(function(teamResult){
       	console.log(ctx, 'get team ', teamResult);
+	$scope.dashboard.team = teamResult.Team.Name;
 	$scope.dashboard.ntournaments = teamResult.Team.TournamentIds.length;
 	$scope.dashboard.nmembers = teamResult.Players.length;
 	$scope.dashboard.accuracy = teamResult.Team.Accuracy;
