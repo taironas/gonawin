f4a93ebaa438af1cc2cb6b95689735022eac5479
Remy Jourde <remy.jourde@gmail.com>
2014-03-23 18:00:18 +0100

    Create cards for teams view
    Fixes #351

--- static/js/controllers/teamControllers.js
+++ static/js/controllers/teamControllers.js
@@ -6,12 +6,12 @@ teamControllers.controller('TeamListCtrl', ['$rootScope', '$scope', 'Team', 'Use
   function($rootScope, $scope, Team, User, $location) {
     console.log('Team list controller:');
     $scope.teams = Team.query();
-  
+
     $scope.teams.$promise.then(function(result){
       if(!$scope.teams || ($scope.teams && !$scope.teams.length))
         $scope.noTeamsMessage = 'You have no teams';
     });
-    
+
     $rootScope.currentUser.$promise.then(function(currentUser){
       var userData = User.get({ id:currentUser.User.Id, including: "Teams" });
       console.log('user data = ', userData);
@@ -19,20 +19,40 @@ teamControllers.controller('TeamListCtrl', ['$rootScope', '$scope', 'Team', 'Use
         $scope.joinedTeams = result.Teams;
       });
     });
-  
+
     $scope.searchTeam = function(){
       console.log('TeamListCtrl: searchTeam');
       console.log('keywords: ', $scope.keywords);
       $location.search('q', $scope.keywords).path('/teams/search');
     };
+
+    $scope.showMoreTeams = false;
+    $scope.moreTeams = function() {
+      $scope.showMoreTeams = true;
+    };
+
 }]);
+
+teamControllers.controller('TeamCardCtrl', ['$scope', 'Team',
+  function($scope, Team) {
+    console.log('Team card controller:');
+
+    $scope.teamData = Team.get({ id:$scope.$parent.team.Id});
+
+    $scope.teamData.$promise.then(function(teamData){
+      $scope.team = teamData.Team;
+      console.log('team card controller, teamData = ', teamData);
+      $scope.membersCount = teamData.Players.length;
+    });
+}]);
+
 
 teamControllers.controller('TeamSearchCtrl', ['$scope', '$routeParams', 'Team', '$location', function($scope, $routeParams, Team, $location) {
   console.log('Team search controller');
   console.log('routeParams: ', $routeParams);
   // get teams result from search query
   $scope.teamsData = Team.search( {q:$routeParams.q});
-  
+
   $scope.query = $routeParams.q;
   // use the isSearching mode to differientiate:
   // no teams in app AND no teams found using query search
@@ -42,7 +62,7 @@ teamControllers.controller('TeamSearchCtrl', ['$scope', '$routeParams', 'Team', 
     $scope.teams = result.Teams;
     $scope.messageInfo = result.MessageInfo;
   });
-   
+
   $scope.searchTeam = function(){
     console.log('TeamSearchCtrl: searchTeam');
     console.log('keywords: ', $scope.keywords);
@@ -54,19 +74,19 @@ teamControllers.controller('TeamNewCtrl', ['$scope', 'Team', '$location', functi
   $scope.addTeam = function() {
     console.log('TeamNewCtrl: AddTeam');
     Team.save($scope.team,
-	      function(team) {
-		$location.path('/teams/' + team.Id);
-	      },
-	      function(err) {
-		$scope.messageDanger = err.data;
-		console.log('save failed: ', err.data);
-	      });
+      function(team) {
+	      $location.path('/teams/' + team.Id);
+      },
+      function(err) {
+        $scope.messageDanger = err.data;
+	      console.log('save failed: ', err.data);
+      });
   };
 }]);
 
 teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$location', '$q', function($scope, $routeParams, Team, $location, $q) {
   $scope.teamData = Team.get({ id:$routeParams.id });
-  
+
   $scope.deleteTeam = function() {
     Team.delete({ id:$routeParams.id },
 		function(){
@@ -126,7 +146,7 @@ teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$
         $scope.joinButtonName = 'Leave';
         $scope.joinButtonMethod = $scope.leaveTeam;
       } );
-      
+
     });
   };
 
@@ -139,7 +159,7 @@ teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$
       });
     });
   };
-  
+
   $scope.teamData.$promise.then(function(teamResult){
     var deferred = $q.defer();
     if (teamResult.Joined) {
@@ -152,7 +172,7 @@ teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$
   }).then(function(result){
     $scope.joinButtonName = result;
   });
-  
+
   $scope.teamData.$promise.then(function(teamResult){
     var deferred = $q.defer();
     if (teamResult.Joined) {
@@ -169,7 +189,7 @@ teamControllers.controller('TeamShowCtrl', ['$scope', '$routeParams', 'Team', '$
 
 teamControllers.controller('TeamEditCtrl', ['$scope', '$routeParams', 'Team', '$location', function($scope, $routeParams, Team, $location) {
   $scope.teamData = Team.get({ id:$routeParams.id });
-  
+
   $scope.updateTeam = function() {
     var teamData = Team.get({ id:$routeParams.id });
     Team.update({ id:$routeParams.id }, $scope.teamData.Team,
@@ -210,5 +230,5 @@ teamControllers.controller('TeamAccuracyByTournamentCtrl', ['$scope', '$routePar
   console.log('route params', $routeParams);
   $scope.teamData = Team.get({ id:$routeParams.id });
   $scope.accuracyData = Team.accuracy({id:$routeParams.id, tournamentId:$routeParams.tournamentId});
-  
+
 }]);
