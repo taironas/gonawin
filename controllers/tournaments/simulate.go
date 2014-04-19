88016ad22eb4579053556512c8786749e6a6babd
santiaago <santiago.ariassar@gmail.com>
2014-04-19 12:16:56 +0200

    simulate a phase now publish activities for matches. Fixes #389

--- controllers/tournaments/simulate.go
+++ controllers/tournaments/simulate.go
@@ -17,10 +17,12 @@
 package tournaments
 
 import (
-	"appengine"
 	"errors"
+	"fmt"
 	"math/rand"
 	"net/http"
+
+	"appengine"
 
 	"github.com/santiaago/purple-wing/helpers"
 	"github.com/santiaago/purple-wing/helpers/handlers"
@@ -30,6 +32,7 @@ import (
 )
 
 // Simulate the scores of a phase in a tournament.
+//    POST /j/tournaments/[0-9]+/matches/simulate?phase=:phaseName
 func SimulateMatches(w http.ResponseWriter, r *http.Request, u *mdl.User) error {
 	c := appengine.NewContext(r)
 
@@ -78,6 +81,21 @@ func SimulateMatches(w http.ResponseWriter, r *http.Request, u *mdl.User) error 
 		if err = mdl.SetResults(c, matches, results1, results2, t); err != nil {
 			log.Errorf(c, "Tournament Simulate Matches: unable to set result for matches error: %v", err)
 			return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeMatchesCannotUpdate)}
+		}
+
+		// publish activities:
+		for i, match := range matches {
+			object := mdl.ActivityEntity{Id: match.TeamId1, Type: "tteam", DisplayName: mapIdTeams[match.TeamId1]}
+			target := mdl.ActivityEntity{Id: match.TeamId2, Type: "tteam", DisplayName: mapIdTeams[match.TeamId2]}
+			verb := ""
+			if results1[i] > results2[i] {
+				verb = fmt.Sprintf("won %d-%d against", results1[i], results2[i])
+			} else if results1[i] < results2[i] {
+				verb = fmt.Sprintf("loose %d-%d against", results1[i], results2[i])
+			} else {
+				verb = fmt.Sprintf("tied %d-%d against", results1[i], results2[i])
+			}
+			t.Publish(c, "match", verb, object, target)
 		}
 
 		if phaseId >= 0 {
