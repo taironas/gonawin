3d0c4d72f061dec7dad8b99e06dd220304683ad7
Remy Jourde <remy.jourde@gmail.com>
2014-05-18 15:12:58 +0200

    Set user dashboard as default
    Fixes #479

--- gonawin/static/js/controllers/dashboardControllers.js
+++ gonawin/static/js/controllers/dashboardControllers.js
@@ -3,14 +3,15 @@
 // Dashboard controllers update the dashboard information depending on the user location.
 var dashboardControllers = angular.module('dashboardControllers', []);
 
-dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', 'Session', 'sAuth', 'User', 'Team', 'Tournament', '$route', function ($scope, $rootScope, $routeParams, $location, $cookieStore, Session, sAuth, User, Team, Tournament, $route) {
+dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', 'Session', 'sAuth', 'User', 'Team', 'Tournament', '$route',
+  function ($scope, $rootScope, $routeParams, $location, $cookieStore, Session, sAuth, User, Team, Tournament, $route) {
     console.log('Dashboard module');
     console.log('DashboardCtrl, current user = ', $rootScope.currentUser);
     console.log('DashboardCtrl, isLoggedIn = ', $rootScope.isLoggedIn);
     $scope.dashboard = {};
     $scope.predicate = 'Score';
     $scope.state = '';
-    
+
     // Set the dashboard data with respect of the url:
     // Current urls watches:
     // - root: /
@@ -23,251 +24,241 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
     // On RouteParams:
     //
     //   We are not able to get the current team id with $routeParams all the time, so we use $route when $routeParams does not work.
-    //   from angular documentation: 
+    //   from angular documentation:
     //   http://docs.angularjs.org/api/ngRoute/service/$routeParams
-    //     Note that the $routeParams are only updated after a route change completes successfully. 
-    //     This means that you cannot rely on $routeParams being correct in route resolve functions. 
+    //     Note that the $routeParams are only updated after a route change completes successfully.
+    //     This means that you cannot rely on $routeParams being correct in route resolve functions.
     //     Instead you can use $route.current.params to access the new route's parameters.
     //
     $scope.setDashboard = function(){
-	var ctx = 'set dashboard: ';
-	console.log(ctx, 'location - abs url: ', $location.absUrl());
-	console.log(ctx, 'location - url: ', $location.url());
-	console.log(ctx, 'location - path: ', $location.path());
+      var ctx = 'set dashboard: ';
+      console.log(ctx, 'location - abs url: ', $location.absUrl());
+      console.log(ctx, 'location - url: ', $location.url());
+      console.log(ctx, 'location - path: ', $location.path());
 
-	var url = $location.url();
-	console.log(ctx, 'match root? ', url.match('^/$') != null || url.match('^/users/[0-9]+.*') || url.match('^/about$') || url.match('^/search$') != null);
-	console.log(ctx, 'match tournaments? ', url.match('^/tournaments/[0-9]+.*') != null);
-	console.log(ctx, 'match teams? ', url.match('^/teams/[0-9]+.*') != null);
-	console.log(ctx, 'route:--->', $route);
+      var url = $location.url();
+      console.log(ctx, 'match tournaments? ', url.match('^/tournaments/[0-9]+.*') != null);
+      console.log(ctx, 'match teams? ', url.match('^/teams/[0-9]+.*') != null);
+      console.log(ctx, 'route:--->', $route);
 
-	if(url.match('^/$') != null || url.match('^/users/[0-9]+.*') || url.match('^/about$') || url.match('^/search$') != null){
-	    $scope.state = 'root';
+      if(url.match('^/tournaments/[0-9]+.*') != null){
+        // if same state as before just exit.
+        if($scope.state == 'tournaments'){
+          console.log('same!');
+          return;
+        }
+        $scope.state = 'tournaments';
 
-	    // reset dashboard before getting data
-	    $scope.dashboard = {};
+        // reset dashboard before getting data
+        $scope.dashboard = {};
+        $scope.dashboard.context = 'tournament';
 
-	    $scope.dashboard.location = 'root';
-	    $scope.dashboard.context = 'user';
-	    $rootScope.currentUser.$promise.then(function(currentUser){
-		$scope.dashboard.user = currentUser.User.Name;
-		$scope.dashboard.name = currentUser.User.Username;
-		$scope.dashboard.userid = currentUser.User.Id;
-		if (currentUser.User.TournamentIds){
-		    $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
-		} else {
-		    $scope.dashboard.ntournaments = 0;
-		}
-		
-		if (currentUser.User.TeamIds){
-		    $scope.dashboard.nteams = currentUser.User.TeamIds.length;
-		}else{
-		    $scope.dashboard.nteams = 0;
-		}		
-		// get user score information:
-		$scope.dashboard.score = currentUser.User.Score;
-	    });
-	} else if(url.match('^/tournaments/[0-9]+.*') != null){
-	    // if same state as before just exit.
-	    if($scope.state == 'tournaments'){
-		console.log('same!');
-		return;
-	    }
-	    // reset dashboard before getting data
-	    $scope.dashboard = {};
+        // depending on how we get here, either by refresh, redirect or following link,
+        // sometimes routeparams will have the tournament id and other times route.current will have it.
+        var tournamentId;
+        if($route.current.params.id != undefined){
+          tournamentId = $route.current.params.id;
+        } else if($routeParams.id != undefined){
+          tournamentId = $routeParams.id;
+        } else {
+          console.log(ctx, 'unable to get tournament id.');
+          return;
+        }
 
-	    $scope.state = 'tournaments';
-	    $scope.dashboard.location = 'tournament';
-	    $scope.dashboard.context = 'tournament';
+        Tournament.get({ id:tournamentId }).$promise.then(function(tournamentResult){
+          console.log(ctx, 'get tournament ', tournamentResult);
+          $scope.dashboard.tournament = tournamentResult.Tournament.Name;
+          $scope.dashboard.tournamentid = tournamentResult.Tournament.Id;
+          $scope.dashboard.name = tournamentResult.Tournament.Name;
 
-	    // depending on how we get here, either by refresh, redirect or following link,
-	    // sometimes routeparams will have the tournament id and other times route.current will have it.
-	    var tournamentId;
-	    if($route.current.params.id != undefined){
-		tournamentId = $route.current.params.id;
-	    } else if($routeParams.id != undefined){
-		tournamentId = $routeParams.id;
-	    } else{
-		console.log(ctx, 'unable to get tournament id.');
-		return;
-	    }
+          if(tournamentResult.Participants){
+            $scope.dashboard.nparticipants = tournamentResult.Participants.length;
+          } else {
+            $scope.dashboard.nparticipants = 0;
+          }
+          if(tournamentResult.Teams){
+            $scope.dashboard.nteams = tournamentResult.Teams.length;
+          } else {
+            $scope.dashboard.nteams = 0;
+          }
+        });
 
-	    Tournament.get({ id:tournamentId }).$promise.then(function(tournamentResult){
-      		console.log(ctx, 'get tournament ', tournamentResult);
-		$scope.dashboard.tournament = tournamentResult.Tournament.Name;
-		$scope.dashboard.tournamentid = tournamentResult.Tournament.Id;
-    $scope.dashboard.name = tournamentResult.Tournament.Name;
+        $scope.dashboard.rank = {};
+        $scope.rankBy = 'users';
+        Tournament.ranking({id:tournamentId, rankby:'users', limit:'10'}).$promise.then(function(rankResult){
+          console.log(ctx, 'get users ranking ', rankResult);
+          $scope.dashboard.rank.users = rankResult.Users;
+        });
+        Tournament.ranking({id:tournamentId, rankby:'teams', limit:'10'}).$promise.then(function(rankResult){
+          console.log(ctx, 'get teams ranking', rankResult);
+          $scope.dashboard.rank.teams = rankResult.Teams;
+        });
 
-		if(tournamentResult.Participants){
-		    $scope.dashboard.nparticipants = tournamentResult.Participants.length;
-		} else{
-		    $scope.dashboard.nparticipants = 0;
-		}
-		if(tournamentResult.Teams){
-		    $scope.dashboard.nteams = tournamentResult.Teams.length;
-		} else{
-		    $scope.dashboard.nteams = 0;
-		}
+        $rootScope.currentUser.$promise.then(function(currentUser){
+          $scope.dashboard.user = currentUser.displayName();
+          $scope.dashboard.userid = currentUser.User.Id;
+        });
+      } else if(url.match('^/tournaments/?$') != null) {
+        // if same state as before just exit.
+        if($scope.state == 'tournamentsindex') {
+          console.log('same!');
+          return;
+        }
+        $scope.state = 'tournamentsindex';
 
-	    });
-	    $scope.dashboard.rank = {};
+        // reset dashboard before getting data
+        $scope.dashboard = {};
+        $scope.dashboard.context = 'tournaments index';
 
-	    $scope.rankBy = 'users';
-	    Tournament.ranking({id:tournamentId, rankby:'users', limit:'10'}).$promise.then(function(rankResult){
-		console.log(ctx, 'get users ranking ', rankResult);
-		$scope.dashboard.rank.users = rankResult.Users;
-	    });
-	    Tournament.ranking({id:tournamentId, rankby:'teams', limit:'10'}).$promise.then(function(rankResult){
-	    	console.log(ctx, 'get teams ranking', rankResult);
-	    	$scope.dashboard.rank.teams = rankResult.Teams;
-	    });
+        $rootScope.currentUser.$promise.then(function(currentUser){
+          $scope.dashboard.user = currentUser.dislpayName();
+	        $scope.dashboard.name = currentUser.displayName();
+          $scope.dashboard.userid = currentUser.User.Id;
 
-	    $rootScope.currentUser.$promise.then(function(currentUser){
-		$scope.dashboard.user = currentUser.User.Name;
-		$scope.dashboard.userid = currentUser.User.Id;
-	    });
-	} else if(url.match('^/tournaments/?$') != null){
+          if (currentUser.User.TournamentIds){
+            $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
+          } else {
+            $scope.dashboard.ntournaments = 0;
+          }
 
-	    // if same state as before just exit.
-	    if($scope.state == 'tournamentsindex'){
-		console.log('same!');
-		return;
-	    }
-	    // reset dashboard before getting data
-	    $scope.dashboard = {};
+          if (currentUser.User.TeamIds){
+            $scope.dashboard.nteams = currentUser.User.TeamIds.length;
+          } else {
+            $scope.dashboard.nteams = 0;
+          }
+          // get user score information:
+          $scope.dashboard.score = currentUser.User.Score;
+          // get user tournaments.
+          User.tournaments({id:$rootScope.currentUser.User.Id}).$promise.then(function(response){
+            $scope.dashboard.tournaments = response.Tournaments;
+          });
+        });
+      } else if(url.match('^/teams/[0-9]+.*') != null) {
+        // if same state as before just exit.
+        if($scope.state == 'teams'){
+          console.log('same!');
+          return;
+        }
+        $scope.state = 'teams';
 
-	    $scope.dashboard.location = 'tournaments index';
-	    $scope.dashboard.context = 'tournaments index';
-	    $scope.state = 'tournamentsindex';
+        // reset dashboard before getting data
+        $scope.dashboard = {};
+        $scope.dashboard.context = 'team';
 
-	    $rootScope.currentUser.$promise.then(function(currentUser){
-		$scope.dashboard.user = currentUser.User.Name;
-		$scope.dashboard.name = currentUser.User.Name;
-		$scope.dashboard.userid = currentUser.User.Id;
-		if (currentUser.User.TournamentIds){
-		    $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
-		} else {
-		    $scope.dashboard.ntournaments = 0;
-		}
-		
-		if (currentUser.User.TeamIds){
-		    $scope.dashboard.nteams = currentUser.User.TeamIds.length;
-		}else{
-		    $scope.dashboard.nteams = 0;
-		}
-		// get user score information:
-		$scope.dashboard.score = currentUser.User.Score;
-		// get user tournaments.
-		User.tournaments({id:$rootScope.currentUser.User.Id}).$promise.then(function(response){
-		    $scope.dashboard.tournaments = response.Tournaments;
-		});
+        var teamId;
+        if($route.current.params.id != undefined){
+          teamId = $route.current.params.id;
+        } else if($routeParams.id != undefined) {
+          teamId = $routeParams.id;
+        } else {
+          console.log(ctx, 'unable to get tournament id.');
+          return;
+        }
 
-	    });
+        $rootScope.currentUser.$promise.then(function(currentUser) {
+          $scope.dashboard.user = currentUser.displayName();
+          $scope.dashboard.userid = currentUser.User.Id;
+        });
 
-	} else if(url.match('^/teams/[0-9]+.*') != null){
-	    $scope.state = 'teams';
+        console.log(ctx, 'route', $route);
+        Team.get({ id:teamId }).$promise.then(function(teamResult) {
+          console.log(ctx, 'get team ', teamResult);
+          $scope.dashboard.team = teamResult.Team.Name;
+          $scope.dashboard.teamid = teamResult.Team.Id;
+          $scope.dashboard.name = teamResult.Team.Name;
 
-	    // reset dashboard before getting data
-	    $scope.dashboard = {};
+          if(teamResult.Team.TournamentIds){
+            $scope.dashboard.ntournaments = teamResult.Team.TournamentIds.length;
+          } else {
+            $scope.dashboard.ntournaments = 0;
+          }
 
-	    $scope.dashboard.location = 'team';
-	    $scope.dashboard.context = 'team';
+          if(teamResult.Players){
+            $scope.dashboard.nmembers = teamResult.Players.length;
+          } else {
+            $scope.dashboard.nmembers = 0;
+          }
 
-	    var teamId;
-	    if($route.current.params.id != undefined){
-		teamId = $route.current.params.id;
-	    } else if($routeParams.id != undefined){
-		teamId = $routeParams.id;
-	    } else{
-		console.log(ctx, 'unable to get tournament id.');
-		return;
-	    }
+          $scope.dashboard.accuracy = teamResult.Team.Accuracy;
+        });
 
+        Team.ranking({id:teamId, limit:'10'}).$promise.then(function(rankResult){
+          console.log(ctx, 'get team ranking', rankResult);
+          $scope.dashboard.members = rankResult.Users;
+        });
+      } else if(url.match('^/teams/?$') != null) {
+        // if same state as before just exit.
+        if($scope.state == 'teamsindex') {
+          console.log('same!');
+          return;
+        }
+        $scope.state = 'teamsindex';
 
-	    $rootScope.currentUser.$promise.then(function(currentUser){
-		$scope.dashboard.user = currentUser.User.Name;
-		$scope.dashboard.userid = currentUser.User.Id;
-	    });
+        // reset dashboard before getting data
+        $scope.dashboard = {};
+        $scope.dashboard.context = 'teams index';
 
-	    console.log(ctx, 'route', $route);
-	    Team.get({ id:teamId }).$promise.then(function(teamResult){
-      		console.log(ctx, 'get team ', teamResult);
-		$scope.dashboard.team = teamResult.Team.Name;
-		$scope.dashboard.teamid = teamResult.Team.Id;
-    $scope.dashboard.name = teamResult.Team.Name;
+        $rootScope.currentUser.$promise.then(function(currentUser) {
+          $scope.dashboard.user = currentUser.displayName();
+          $scope.dashboard.name = currentUser.displayName();
+          $scope.dashboard.userid = currentUser.User.Id;
 
-		if(teamResult.Team.TournamentIds){
-		    $scope.dashboard.ntournaments = teamResult.Team.TournamentIds.length;
-		}else{
-		    $scope.dashboard.ntournaments = 0;
-		}
+          if (currentUser.User.TournamentIds){
+            $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
+          } else {
+            $scope.dashboard.ntournaments = 0;
+          }
 
-		if(teamResult.Players){
-		    $scope.dashboard.nmembers = teamResult.Players.length;
-		}else{
-		    $scope.dashboard.nmembers = 0;
-		}
+          if (currentUser.User.TeamIds){
+            $scope.dashboard.nteams = currentUser.User.TeamIds.length;
+          } else {
+            $scope.dashboard.nteams = 0;
+          }
+          // get user score information:
+          $scope.dashboard.score = currentUser.User.Score;
 
-		$scope.dashboard.accuracy = teamResult.Team.Accuracy;
-	    });
+          // get user teams.
+          User.teams({id:$rootScope.currentUser.User.Id}).$promise.then(function(response){
+            $scope.dashboard.teams = response.Teams;
+          });
+        });
+      // Default dashboard: user
+      } else {
+        $scope.state = 'user';
+        // reset dashboard before getting data
+        $scope.dashboard = {};
+        $scope.dashboard.context = 'user';
 
-	    Team.ranking({id:teamId, limit:'10'}).$promise.then(function(rankResult){
-		console.log(ctx, 'get team ranking', rankResult);
-		$scope.dashboard.members = rankResult.Users;
-	    });
-	} else if(url.match('^/teams/?$') != null){
-	    // if same state as before just exit.
-	    if($scope.state == 'teamsindex'){
-		console.log('same!');
-		return;
-	    }
-	    // reset dashboard before getting data
-	    $scope.dashboard = {};
+        $rootScope.currentUser.$promise.then(function(currentUser){
+          $scope.dashboard.user = currentUser.User.Name;
+          $scope.dashboard.name = currentUser.displayName();
+          $scope.dashboard.userid = currentUser.User.Id;
 
-	    $scope.state = 'teamsindex';
-	    $scope.dashboard.location = 'teams index';
-	    $scope.dashboard.context = 'teams index';
+          if (currentUser.User.TournamentIds){
+            $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
+          } else {
+            $scope.dashboard.ntournaments = 0;
+          }
 
-	    $rootScope.currentUser.$promise.then(function(currentUser){
-		console.log('current user!!!!!!', currentUser);
-		$scope.dashboard.user = currentUser.User.Name;
-		$scope.dashboard.name = currentUser.User.Name;
-		$scope.dashboard.userid = currentUser.User.Id;
-		if (currentUser.User.TournamentIds){
-		    $scope.dashboard.ntournaments = currentUser.User.TournamentIds.length;
-		} else {
-		    $scope.dashboard.ntournaments = 0;
-		}
-		
-		if (currentUser.User.TeamIds){
-		    $scope.dashboard.nteams = currentUser.User.TeamIds.length;
-		}else{
-		    $scope.dashboard.nteams = 0;
-		}
-		// get user score information:
-		$scope.dashboard.score = currentUser.User.Score;
-
-		// get user teams.
-		User.teams({id:$rootScope.currentUser.User.Id}).$promise.then(function(response){
-		    $scope.dashboard.teams = response.Teams;
-		});
-
-	    });
-
-	} else {
-	    $scope.dashboard.location = 'default';
-	    $scope.dashboard.context = 'default';
-	}
+          if (currentUser.User.TeamIds){
+            $scope.dashboard.nteams = currentUser.User.TeamIds.length;
+          } else{
+            $scope.dashboard.nteams = 0;
+          }
+          // get user score information:
+          $scope.dashboard.score = currentUser.User.Score;
+        });
+      }
     };
 
     $scope.byUsersOnClick = function(){
-	$scope.rankBy = 'users';
-	return;
+      $scope.rankBy = 'users';
+      return;
     };
 
     $scope.byTeamsOnClick = function(){
-	$scope.rankBy = 'teams';
-	return;
+      $scope.rankBy = 'teams';
+      return;
     };
 
     // set dashboard with respect to url in the global controller.
@@ -278,8 +269,7 @@ dashboardControllers.controller('DashboardCtrl', ['$scope', '$rootScope', '$rout
     // $locationChangeSuccess event is triggered when url changes.
     // note: this event is not triggered when page is refreshed.
     $scope.$on('$locationChangeSuccess', function(event) {
-	console.log('location changed:');
-	$scope.setDashboard();
+      console.log('location changed:');
+      $scope.setDashboard();
     });
-
 }]);
--- gonawin/static/js/services/dataServices.js
+++ gonawin/static/js/services/dataServices.js
@@ -2,30 +2,31 @@
 var dataServices = angular.module('dataServices', ['ngResource']);
 
 dataServices.factory('User', function($http, $resource, $cookieStore) {
-    $http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
+  $http.defaults.headers.common['Authorization'] = $cookieStore.get('auth');
 
-    var User = $resource('j/users/:id', {id:'@id', including:'@including', teamId:'@teamId'}, {
-	get: { method: 'GET', params: {including: '@including'}, url: 'j/users/show/:id' },
-	update: { method: 'POST', url: 'j/users/update/:id' },
-	delete: { method: 'POST', url: 'j/users/destroy/:id' },
-	scores: {method: 'GET', url: 'j/users/:id/scores'},
-	search: { method: 'GET', url: 'j/users/search?q=:q', cache : true},
-	teams: {method : 'GET', url: 'j/users/:id/teams'},
-	tournaments: {method : 'GET', url: 'j/users/:id/tournaments'},
-	allowInvitation : {method: 'POST', url: 'j/users/allow/:teamId'},
-	denyInvitation : {method: 'POST', url: 'j/users/deny/:teamId'},
-    })
-    // define display name to handle alias or user name.
-    // Note: There is another displayName function definition in the Session ressource as we handle users via User and Session.
-    User.prototype.displayName = function() {
-	if(this.User == undefined) return;
-	if(this.User.Alias.length > 0){
-	    return this.User.Alias;
-	} else{
-	    return this.User.Username;
-	}
-    };
-    return User;
+  var User = $resource('j/users/:id', {id:'@id', including:'@including', teamId:'@teamId'}, {
+    get: { method: 'GET', params: {including: '@including'}, url: 'j/users/show/:id' },
+    update: { method: 'POST', url: 'j/users/update/:id' },
+    delete: { method: 'POST', url: 'j/users/destroy/:id' },
+    scores: {method: 'GET', url: 'j/users/:id/scores'},
+    search: { method: 'GET', url: 'j/users/search?q=:q', cache : true},
+    teams: {method : 'GET', url: 'j/users/:id/teams'},
+    tournaments: {method : 'GET', url: 'j/users/:id/tournaments'},
+    allowInvitation : {method: 'POST', url: 'j/users/allow/:teamId'},
+    denyInvitation : {method: 'POST', url: 'j/users/deny/:teamId'},
+  })
+  // define display name to handle alias or user name.
+  // Note: There is another displayName function definition in the Session ressource as we handle users via User and Session.
+  User.prototype.displayName = function() {
+    console.log('displayName, user = ', this);
+    if(this.User == undefined) return;
+    if(this.User.Alias.length > 0){
+      return this.User.Alias;
+    } else{
+      return this.User.Username;
+    }
+  };
+  return User;
 });
 
 dataServices.factory('Team', function($http, $resource, $cookieStore) {
