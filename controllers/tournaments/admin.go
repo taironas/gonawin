31736deb452c5e24cf07d9990f0ab1d0ae60845d
santiaago <santiago.ariassar@gmail.com>
2015-04-19 21:28:14 +0100

    tournament ActivatePhase handler refactor tournament retrieval

--- controllers/tournaments/admin.go
+++ controllers/tournaments/admin.go
@@ -276,24 +276,13 @@ func ActivatePhase(w http.ResponseWriter, r *http.Request, u *mdl.User) error {
 	desc := "Tournament activate phase handler:"
 	log.Infof(c, "%v", desc)
 
-	// get tournament id
-	strTournamentId, err := route.Context.Get(r, "tournamentId")
-	if err != nil {
-		log.Errorf(c, "%s error getting tournament id, err:%v", desc, err)
-		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-	}
+	rc := requestContext{c, desc, r}
 
-	var tournamentId int64
-	tournamentId, err = strconv.ParseInt(strTournamentId, 0, 64)
-	if err != nil {
-		log.Errorf(c, "%s error converting tournament id from string to int64, err:%v", desc, err)
-		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-	}
-
+	var err error
 	var tournament *mdl.Tournament
-	if tournament, err = mdl.TournamentById(c, tournamentId); err != nil {
-		log.Errorf(c, "%s tournament not found: %v", desc, err)
-		return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
+
+	if tournament, err = rc.tournament(); err != nil {
+		return err
 	}
 
 	phaseName := r.FormValue("phaseName")
