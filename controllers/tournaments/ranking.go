9cf87ce0051b1e8a9932266c74a58f505c0c536d
santiaago <santiago.ariassar@gmail.com>
2015-04-20 22:00:16 +0100

    tournament ranking exit first if wrong method

--- controllers/tournaments/ranking.go
+++ controllers/tournaments/ranking.go
@@ -41,80 +41,81 @@ import (
 // The response is an array of users.
 //
 func Ranking(w http.ResponseWriter, r *http.Request, u *mdl.User) error {
+	if r.Method == "GET" {
+		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeNotSupported)}
+	}
+
 	c := appengine.NewContext(r)
 	desc := "Tournament Ranking Handler:"
 
-	if r.Method == "GET" {
-		// get tournament id
-		strTournamentId, err := route.Context.Get(r, "tournamentId")
-		if err != nil {
-			log.Errorf(c, "%s error getting tournament id, err:%v", desc, err)
-			return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-		}
+	// get tournament id
+	strTournamentId, err := route.Context.Get(r, "tournamentId")
+	if err != nil {
+		log.Errorf(c, "%s error getting tournament id, err:%v", desc, err)
+		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
+	}
 
-		var tournamentId int64
-		tournamentId, err = strconv.ParseInt(strTournamentId, 0, 64)
-		if err != nil {
-			log.Errorf(c, "%s error converting tournament id from string to int64, err:%v", desc, err)
-			return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-		}
+	var tournamentId int64
+	tournamentId, err = strconv.ParseInt(strTournamentId, 0, 64)
+	if err != nil {
+		log.Errorf(c, "%s error converting tournament id from string to int64, err:%v", desc, err)
+		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
+	}
 
-		var t *mdl.Tournament
-		t, err = mdl.TournamentById(c, tournamentId)
-		if err != nil {
-			log.Errorf(c, "%s tournament with id:%v was not found %v", desc, tournamentId, err)
-			return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-		}
+	var t *mdl.Tournament
+	t, err = mdl.TournamentById(c, tournamentId)
+	if err != nil {
+		log.Errorf(c, "%s tournament with id:%v was not found %v", desc, tournamentId, err)
+		return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
+	}
 
-		rankby := r.FormValue("rankby")
-		// if wrong data, we set rankby to "users"
-		if rankby != "teams" && rankby != "users" {
-			rankby = "users"
-		}
+	rankby := r.FormValue("rankby")
+	// if wrong data, we set rankby to "users"
+	if rankby != "teams" && rankby != "users" {
+		rankby = "users"
+	}
 
-		strlimit := r.FormValue("limit")
-		limit := 100
-		if len(strlimit) > 0 {
-			if n, err := strconv.ParseInt(strlimit, 10, 64); err != nil {
-				log.Infof(c, "%s, unable to parse %v, error:%v", strlimit, err)
-			} else {
-				if n > 0 {
-					limit = int(n)
-				}
+	strlimit := r.FormValue("limit")
+	limit := 100
+	if len(strlimit) > 0 {
+		if n, err := strconv.ParseInt(strlimit, 10, 64); err != nil {
+			log.Infof(c, "%s, unable to parse %v, error:%v", strlimit, err)
+		} else {
+			if n > 0 {
+				limit = int(n)
 			}
-		}
-
-		if rankby == "users" {
-			log.Infof(c, "%s ready to build a user array", desc)
-			users := t.RankingByUser(c, limit)
-
-			fieldsToKeep := []string{"Id", "Username", "Alias", "Score"}
-			usersJson := make([]mdl.UserJson, len(users))
-			helpers.TransformFromArrayOfPointers(&users, &usersJson, fieldsToKeep)
-
-			data := struct {
-				Users []mdl.UserJson
-			}{
-				usersJson,
-			}
-
-			return templateshlp.RenderJson(w, c, data)
-
-		} else if rankby == "teams" {
-			log.Infof(c, "%s ready to build team array", desc)
-			teams := t.RankingByTeam(c, limit)
-
-			fieldsToKeep := []string{"Id", "Name", "Accuracy"}
-			teamsJson := make([]mdl.TeamJson, len(teams))
-			helpers.TransformFromArrayOfPointers(&teams, &teamsJson, fieldsToKeep)
-
-			data := struct {
-				Teams []mdl.TeamJson
-			}{
-				teamsJson,
-			}
-			return templateshlp.RenderJson(w, c, data)
 		}
 	}
-	return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeNotSupported)}
+
+	if rankby == "users" {
+		log.Infof(c, "%s ready to build a user array", desc)
+		users := t.RankingByUser(c, limit)
+
+		fieldsToKeep := []string{"Id", "Username", "Alias", "Score"}
+		usersJson := make([]mdl.UserJson, len(users))
+		helpers.TransformFromArrayOfPointers(&users, &usersJson, fieldsToKeep)
+
+		data := struct {
+			Users []mdl.UserJson
+		}{
+			usersJson,
+		}
+
+		return templateshlp.RenderJson(w, c, data)
+
+	} else if rankby == "teams" {
+		log.Infof(c, "%s ready to build team array", desc)
+		teams := t.RankingByTeam(c, limit)
+
+		fieldsToKeep := []string{"Id", "Name", "Accuracy"}
+		teamsJson := make([]mdl.TeamJson, len(teams))
+		helpers.TransformFromArrayOfPointers(&teams, &teamsJson, fieldsToKeep)
+
+		data := struct {
+			Teams []mdl.TeamJson
+		}{
+			teamsJson,
+		}
+		return templateshlp.RenderJson(w, c, data)
+	}
 }
