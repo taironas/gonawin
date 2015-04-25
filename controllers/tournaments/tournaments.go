b89c192a2abce2a713bdd7f4a02331ef5bdc9531
santiaago <santiago.ariassar@gmail.com>
2015-04-25 20:47:07 +0100

    tournament Participants - refactor tournament retrieval

--- controllers/tournaments/tournaments.go
+++ controllers/tournaments/tournaments.go
@@ -502,34 +502,21 @@ func Participants(w http.ResponseWriter, r *http.Request, u *mdl.User) error {
 
 	c := appengine.NewContext(r)
 	desc := "Tournament Participants handler:"
-
-	// get tournament id
-	strTournamentId, err := route.Context.Get(r, "tournamentId")
-	if err != nil {
-		log.Errorf(c, "%s error getting tournament id, err:%v", desc, err)
-		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-	}
-
-	var tournamentId int64
-	tournamentId, err = strconv.ParseInt(strTournamentId, 0, 64)
-	if err != nil {
-		log.Errorf(c, "%s error converting tournament id from string to int64, err:%v", desc, err)
-		return &helpers.BadRequest{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
-	}
+	rc := requestContext{c, desc, r}
 
 	var tournament *mdl.Tournament
-	tournament, err = mdl.TournamentById(c, tournamentId)
+	var err error
+	tournament, err = rc.tournament()
 	if err != nil {
-		log.Errorf(c, "%s tournament with id:%v was not found %v", desc, tournamentId, err)
-		return &helpers.NotFound{Err: errors.New(helpers.ErrorCodeTournamentNotFound)}
+		return err
 	}
 
 	participants := tournament.Participants(c)
-	// participant
+
 	participantFieldsToKeep := []string{"Id", "Username", "Alias"}
 	participantsJson := make([]mdl.UserJson, len(participants))
 	helpers.TransformFromArrayOfPointers(&participants, &participantsJson, participantFieldsToKeep)
-	// data
+
 	data := struct {
 		Participants []mdl.UserJson
 	}{
