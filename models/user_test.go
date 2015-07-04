03e4e63e9726e304b03cae1868874b1e82b09263
santiaago <santiago.ariassar@gmail.com>
2015-07-04 16:28:44 +0100

    check user entity

--- models/user_test.go
+++ models/user_test.go
@@ -15,30 +15,50 @@ func TestCreateUser(t *testing.T) {
 	defer c.Close()
 
 	tests := []struct {
+		title    string
 		email    string
 		username string
 		name     string
 		alias    string
 		isAdmin  bool
 		auth     string
+		err      string
 	}{
 		{
+			title:    "can create user",
 			email:    "foo@bar.com",
 			username: "john.snow",
 			name:     "john snow",
 			alias:    "",
 			isAdmin:  false,
 			auth:     "",
+			err:      "",
 		},
 	}
 
 	for i, test := range tests {
+		t.Log(test.title)
 		var got *User
 		if got, err = CreateUser(c, test.email, test.username, test.name, test.alias, test.isAdmin, test.auth); err != nil {
 			t.Errorf("test %v - Error: %v", i, err)
 		}
 		if got.Email != test.email {
 			t.Errorf("test %v - Error; want Email == %s, got %s", i, test.email, got.Email)
+		}
+		if got.Username != test.username {
+			t.Errorf("test %v - Error; want Username == %s, got %s", i, test.username, got.Username)
+		}
+		if got.Name != test.name {
+			t.Errorf("test %v - Error; want Name == %s, got %s", i, test.name, got.Name)
+		}
+		if got.Alias != test.alias {
+			t.Errorf("test %v - Error; want Name == %s, got %s", i, test.alias, got.Alias)
+		}
+		if got.IsAdmin != test.isAdmin {
+			t.Errorf("test %v - Error; want isAdmin == %s, got %s", i, test.isAdmin, got.IsAdmin)
+		}
+		if got.Auth != test.auth {
+			t.Errorf("test %v - Error; want auth == %s, got %s", i, test.auth, got.Auth)
 		}
 	}
 }
