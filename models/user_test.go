3a27ca295e03934a6f5c2e2c8e49af576765f9b2
santiaago <santiago.ariassar@gmail.com>
2015-07-04 16:22:57 +0100

    add first test for create user

--- models/user_test.go
+++ models/user_test.go
@@ -1,7 +1,44 @@
 package models
 
-import "testing"
+import (
+	"testing"
+
+	"appengine/aetest"
+)
 
 func TestCreateUser(t *testing.T) {
-	t.Log("test")
+	var c aetest.Context
+	var err error
+	if c, err = aetest.NewContext(nil); err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	tests := []struct {
+		email    string
+		username string
+		name     string
+		alias    string
+		isAdmin  bool
+		auth     string
+	}{
+		{
+			email:    "foo@bar.com",
+			username: "john.snow",
+			name:     "john snow",
+			alias:    "",
+			isAdmin:  false,
+			auth:     "",
+		},
+	}
+
+	for i, test := range tests {
+		var got *User
+		if got, err = CreateUser(c, test.email, test.username, test.name, test.alias, test.isAdmin, test.auth); err != nil {
+			t.Errorf("test %v - Error: %v", i, err)
+		}
+		if got.Email != test.email {
+			t.Errorf("test %v - Error; want Email == %s, got %s", i, test.email, got.Email)
+		}
+	}
 }
