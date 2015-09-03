bf783ea49edba1886b81811d56aa136f6ea30e87
Remy Jourde <remy.jourde@gmail.com>
2015-09-03 09:21:04 +0200

    add test User.UsersByIds

--- models/user_test.go
+++ models/user_test.go
@@ -53,6 +53,111 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
+// TestUserById tests that you can get a user by its ID.
+//
+func TestUserById(t *testing.T) {
+	var c aetest.Context
+	var err error
+	options := aetest.Options{StronglyConsistentDatastore: true}
+
+	if c, err = aetest.NewContext(&options); err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	test := struct {
+		title string
+		user  testUser
+	}{
+		"can get user by ID", testUser{"foo@bar.com", "john.snow", "john snow", "crow", false, ""},
+	}
+
+	t.Log(test.title)
+	var got *User
+	if got, err = CreateUser(c, test.user.email, test.user.username, test.user.name, test.user.alias, test.user.isAdmin, test.user.auth); err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	var u *User
+
+	// Test non existing user
+	if u, err = UserById(c, got.Id+50); u != nil {
+		t.Errorf("Error: no user should have been found")
+	}
+
+	if err == nil {
+		t.Errorf("Error: an error should have been returned in case of non existing user")
+	}
+
+	// Test existing user
+	if u, err = UserById(c, got.Id); u == nil {
+		t.Errorf("Error: user not found")
+	}
+
+	if err = checkUser(got, test.user); err != nil {
+		t.Errorf("Error: want user == %v, got %v", test.user, got)
+	}
+}
+
+// TestUsersByIds tests that you can get a list of users by their IDs.
+//
+func TestUsersByIds(t *testing.T) {
+	var c aetest.Context
+	var err error
+	options := aetest.Options{StronglyConsistentDatastore: true}
+
+	if c, err = aetest.NewContext(&options); err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	test := struct {
+		title string
+		users []testUser
+	}{
+		"can get users by IDs",
+		[]testUser{
+			{"foo@bar.com", "john.snow", "john snow", "crow", false, ""},
+			{"foo@bar.com", "robb.stark", "robb stark", "king in the north", false, ""},
+			{"foo@bar.com", "jamie.lannister", "jamie lannister", "kingslayer", false, ""},
+		},
+	}
+
+	t.Log(test.title)
+	var gotIDs []int64
+	var got *User
+	for _, user := range test.users {
+		if got, err = CreateUser(c, user.email, user.username, user.name, user.alias, user.isAdmin, user.auth); err != nil {
+			t.Errorf("Error: %v", err)
+		}
+
+		gotIDs = append(gotIDs, got.Id)
+	}
+
+	var users []*User
+
+	// Test non existing users
+	var nonExistingIDs []int64
+	for _, ID := range gotIDs {
+		nonExistingIDs = append(nonExistingIDs, ID+50)
+	}
+
+	if users, err = UsersByIds(c, nonExistingIDs); users != nil {
+		t.Errorf("Error: no users should have been found")
+	}
+
+	// Test existing users
+	if users, err = UsersByIds(c, gotIDs); users == nil {
+		t.Errorf("Error: users not found")
+	}
+
+	for i, user := range test.users {
+		if err = checkUser(users[i], user); err != nil {
+			t.Errorf("test %v - Error: %v", i, err)
+		}
+	}
+}
+
 // TestDestroyUser tests that you can destroy a user.
 //
 func TestDestroyUser(t *testing.T) {
