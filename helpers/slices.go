fbab125cb6f9509339c5ebefc13b53b90f06dcbf
Remy Jourde <remy.jourde@gmail.com>
2015-10-29 11:53:59 +0100

    add Contains function

--- helpers/slices.go
+++ helpers/slices.go
@@ -16,11 +16,22 @@
 
 package helpers
 
-// insert new value into a slice at a given position
+// Insert inserts new value into a slice at a given position
 // source: https://code.google.com/p/go-wiki/wiki/SliceTricks
 func Insert(s []int64, value int64, i int64) []int64 {
 	s = append(s, 0)
 	copy(s[i+1:], s[i:])
 	s[i] = value
 	return s
+}
+
+// Contain indicates if a value exists in a given slice.
+// If the value exists, its position in the slice is returned otherwise -1.
+func Contain(s []int64, value int64) (bool, int) {
+	for i, v := range s {
+		if v == value {
+			return true, i
+		}
+	}
+	return false, -1
 }
