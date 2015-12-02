3ffabcb74de50b20ccf00c704f19fd4355ce79a0
Remy Jourde <remy.jourde@gmail.com>
2015-12-02 11:11:30 +0100

    create user helpers test

--- models/user_test.go
+++ models/user_test.go
@@ -1,26 +1,15 @@
 package models
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/taironas/gonawin/helpers"
-	"github.com/taironas/gonawin/test"
-
 	"appengine/aetest"
-)
 
-type testUser struct {
-	email    string
-	username string
-	name     string
-	alias    string
-	isAdmin  bool
-	auth     string
-}
+	"github.com/taironas/gonawin/test"
+)
 
 // TestCreateUser tests that you can create a user.
 //
@@ -954,65 +943,5 @@ func TestUserRemoveTournamentId(t *testing.T) {
 		} else if test.err == "" && contains {
 			t.Errorf("Error: tournament IDs should be empty")
 		}
-	}
-}
-
-func checkUser(got *User, want testUser) error {
-	var s string
-	if got.Email != want.email {
-		s = fmt.Sprintf("want Email == %s, got %s", want.email, got.Email)
-	} else if got.Username != want.username {
-		s = fmt.Sprintf("want Username == %s, got %s", want.username, got.Username)
-	} else if got.Name != want.name {
-		s = fmt.Sprintf("want Name == %s, got %s", want.name, got.Name)
-	} else if got.Alias != want.alias {
-		s = fmt.Sprintf("want Alias == %s, got %s", want.alias, got.Alias)
-	} else if got.IsAdmin != want.isAdmin {
-		s = fmt.Sprintf("want isAdmin == %t, got %t", want.isAdmin, got.IsAdmin)
-	} else {
-		return nil
-	}
-	return errors.New(s)
-}
-
-// checkUserInvertedIndex checks that the user is present in the datastore when
-// performing a search.
-//
-func checkUserInvertedIndex(t *testing.T, c aetest.Context, got *User, want testUser) error {
-
-	var ids []int64
-	var err error
-	words := helpers.SetOfStrings(want.username)
-	if ids, err = GetUserInvertedIndexes(c, words); err != nil {
-		return fmt.Errorf("failed calling GetUserInvertedIndexes %v", err)
-	}
-	for _, id := range ids {
-		if id == got.Id {
-			return nil
-		}
-	}
-
-	return errors.New("user not found")
-
-}
-
-func createNonSavedUser(email, username, name, alias string, isAdmin bool) User {
-	return User{
-		5,
-		email,
-		username,
-		name,
-		alias,
-		isAdmin,
-		"",
-		[]int64{},
-		[]int64{},
-		[]int64{},
-		[]int64{},
-		[]int64{},
-		0,
-		[]ScoreOfTournament{},
-		[]int64{},
-		time.Now(),
 	}
 }
